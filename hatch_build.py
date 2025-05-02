9313100b14447840fafb8b60af67f16f4009e264
David Heejong Park <hee.jong.park@dnv.com>
2025-05-02 18:25:17 +0200

    Update action

--- hatch_build.py
+++ hatch_build.py
@@ -9,7 +9,7 @@ class WheelHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         super().initialize(version, build_data)
         system_os = platform.system()
-        os.system("conan remote add osp https://osp.jfrog.io/artifactory/api/conan/conan-local --force --index 0")
+        os.system("conan remote add osp https://osp.jfrog.io/artifactory/api/conan/conan-local --force")
         os.system("conan profile detect --force")
         assert os.system("conan install . -u -b missing -of build") == 0, "Conan install failed"
         if system_os == "Linux":
