f9cbe0db657d89d088b4850bfb645e3fa249c9ac
David Heejong Park <hee.jong.park@dnv.com>
2025-05-22 08:42:24 +0200

    Build b2

--- hatch_build.py
+++ hatch_build.py
@@ -29,9 +29,9 @@ class WheelHook(BuildHookInterface):
         else:
             build_packages = "-b missing"
 
-        assert os.system(f"conan install . -u {build_packages} -of build --format json -b b2/* --out-file graph.json") == 0, (
-            "Conan install failed"
-        )
+        assert (
+            os.system(f"conan install . -u {build_packages} -of build --format json -b b2/* --out-file graph.json") == 0
+        ), "Conan install failed"
 
         if "CONAN_UPLOAD_OSP" in os.environ:
             print("Uploading packages..")
